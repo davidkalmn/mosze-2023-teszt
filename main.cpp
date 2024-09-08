55319fda3681a37e08abcda961a9c9f35c9675da
mauzileu <kaldavai26@gmail.com>
2024-09-08 13:52:31 +0200

    Hibák kommentelése a fájlban

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibás a konstans neve, N_ELEMENTS kellene, hogy legyen
+    std::cout << '1-100 ertekek duplazasa' //nincsen ; a sor végén
+    for (int i = 0;) //hiányzik a feltétel, illetve a léptetés
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //mindig hamis lesz a feltétel, mert 0-ról kezd az i
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //nincs kiirandó érték, hiányzik a változó
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //nincs inicializálva
+    for (int i = 0; i < N_ELEMENTS, i++) //; kellene a , helyett
     {
-        atlag += b[i]
+        atlag += b[i] //megint nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0; 
+    //nincs memória felszabadítva
 }
